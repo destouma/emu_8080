a7ba19f394c07ec963eafcf877900d7cd185d54c
destouma <mdsoftengr@gmail.com>
2022-10-25 10:39:06 -0400

    first commit

---
+++ 8080emu.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int Emulate8080(unsigned char *codebuffer, int pc){
+    return pc++;
+}
+
+int main (int argc, char**argv){
+	FILE *f= fopen(argv[1], "rb");
+	if (f==NULL)
+	{
+		printf("error: %s file not found\n", argv[1]);
+		exit(1);
+	}
+	
+	//Get the file size
+	fseek(f, 0L, SEEK_END);
+	int fsize = ftell(f);
+	fseek(f, 0L, SEEK_SET);
+
+	unsigned char *buffer=malloc(fsize);
+	fread(buffer, fsize, 1, f);
+	fclose(f);
+
+	int pc = 0;
+	while (pc < fsize)
+	{
+		pc += Emulate8080(buffer, pc);
+		printf ("\n");
+	}
+	return 0;
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+Usefull resources
+-----------------
+
+* Emulator tutorial: 
+  * http://www.emulator101.com/
+* Spaces invaders code explained: 
+  * http://computerarcheology.com/Arcade/SpaceInvaders/Code.html
+* 8080 Assembler
+  * [8080 Programmers Manual](8080%20Programmers%20Manual.pdf)
+* Space invaders ROM:
+  * [invaders.e](invaders.e)
+  * [invaders.f](invaders.f)
+  * [invaders.g](invaders.g)
+  * [invaders.h](invaders.h)
+* 8080 CPU diag adm and bin
+  * [cupdiag.asm](cpudiag.asm)
+  * [cpudiag.bin](cpudiag.bin)
